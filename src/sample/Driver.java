c57b9664f5318e893a3ab0f2ae0b498f00a2a64a
Smith <pentopia11@gmail.com>
2020-11-08 23:47:49 -0800

    i understand incrementally more but not much

--- src/sample/Driver.java
+++ src/sample/Driver.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
 import javafx.stage.Stage;
 
 public class Driver extends Application {
@@ -11,8 +12,10 @@ public class Driver extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("NewBird - Admin Panel");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+
         primaryStage.show();
     }
 
--- src/sample/UserGroup.java
+++ src/sample/UserGroup.java
@@ -2,9 +2,14 @@
 
 package sample;
 
+import java.util.ArrayList;
+
 public class UserGroup implements UserInterface {
     private String id;
     private UserGroup parent;
+    private ArrayList<UserGroup> subgroups = new ArrayList<UserGroup>();
+    private ArrayList<User> users = new ArrayList<User>();
+
 
     @Override
     public String getId() {
